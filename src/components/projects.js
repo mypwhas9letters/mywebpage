e9914396468b5dbb530754206ba9166de6099a6c
Hui Wang <huiw3939@gmail.com>
2018-01-02 18:34:14 -0500

    added new project

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,6 +10,39 @@ class Projects extends React.Component{
 
           <div className="item">
             <div className="image">
+              <h1 className="centerAndSize">Bit Regret</h1>
+              <img src={require("../images/projectLogos/bitregret.png")} alt=""/>
+            </div>
+            <div className="middle aligned content">
+              <div className="ui top attached segment">
+                <p>Bit Regret: I shoud've bought Bitcoin.</p>
+                <p>
+                  I started learning about bitcoin around the same time I started leaning to code. I started my coding bootcamp on 7-17-17 and paid $15,000 in tuition. If I used that money to buy bitcoin on 7-17-17, it would be worth $127,785.23 today, 12/20/2017. Today I'm sitting there with the ability to create this project instead of $127,785.23 dollars. So, I created this app to spread my misery. This app shows you how much money you could've made if you bought bitcoin from an earlier date.
+                </p>
+              </div>
+              <div className="ui attached segment">
+                <button className="ui primary button">
+                  <a href="https://bitregret.cc" target="_blank" rel="noopener noreferrer"><span style={{color: 'white'}}>Link to Project</span></a>
+                </button>
+                <button className="ui black button">
+                  <i className="github icon"></i>
+                  <a href="https://github.com/mypwhas9letters/bit-regret"><span style={{color: 'white'}}>Github Repo</span></a>
+                </button>
+              </div>
+              <h4 className="ui bottom attached segment">
+                <div className="ui tiny images">
+                  <img src={require("../images/logos/react.png")} alt=""/>
+                  <img src={require("../images/logos/redux.png")} alt=""/>
+                  <img src={require("../images/logos/javascript.png")} alt=""/>
+                  <img src={require("../images/logos/html.png")} alt=""/>
+                  <img src={require("../images/logos/css.svg")} alt=""/>
+                </div>
+              </h4>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="image">
               <h1 className="projectTitle">Parallel</h1>
               <img src={require("../images/projectLogos/logo.png")} alt=""/>
             </div>
