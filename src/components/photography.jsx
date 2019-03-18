c08519fee53e3d318f6dc1d9308ee970b9591086
mypwhas9letters <huiw3939@gmail.com>
2019-03-18 15:49:21 -0400

    fixing api call

--- src/components/photography.jsx
+++ src/components/photography.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+const API_KEY = process.env.REACT_APP_API_KEY
 
 class Photography extends Component{
   state = {
@@ -7,7 +8,7 @@ class Photography extends Component{
 
   componentDidMount = async () => {
     try {
-      const url = ''
+      const url = `https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${API_KEY}&user_id=145832077%40N07&format=json&nojsoncallback=1`
       const listOfPhotos = await fetch(url)
       var json = await listOfPhotos.json()
       this.setState({photos: json.photos.photo})
